d6856715c372036bed7b49cd5ddd76167b8c0012
GrandMaster098 Lightyear <sujit.gda997@gmail.com>
2023-04-08 07:12:49 +0000

    image

--- site/src/App.js
+++ site/src/App.js
@@ -1,15 +1,23 @@
-import React from 'react'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Service from './components/Service'
 import About from './components/About'
 import Contact from './components/Contact'
+import Navbaar from './components/Navbaar'
 const App = () => {
   return <>
 
-  <Home />
-  <Service />
-  <About />
-  <Contact />
+  <BrowserRouter>
+    <Navbaar />
+  <Routes>
+    <Route path='/' element={<Home /> }/>
+    <Route path='/service' element={<Service /> }/>
+    <Route path='/about' element={<About /> }/>
+    <Route path='/contact' element={<Contact /> }/>
+  </Routes>
+  </BrowserRouter>
+
+  
   
   </>
 }
--- site/src/components/Home.js
+++ site/src/components/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import '../App.css'
 const Home = () => {
   return (
-    <div>home</div>
+    <div className='hero-section'>
+      <div className='container'>
+        <div className='row'>
+          <div className='col content'>
+
+          </div>
+          <div className='col image'>
+            <img src='./image/background.svg' alt='' />
+            
+
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
